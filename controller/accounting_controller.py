f8b4167ad7c156fd012c7dae876fdb19173fb431
David <cichy.dawid@gmail.com>
2019-03-13 12:21:36 +0100

    accounting_controller done

--- controller/accounting_controller.py
+++ controller/accounting_controller.py
@@ -2,7 +2,17 @@
 from view import terminal_view
 from model.accounting import accounting
 from controller import common
+""" Accounting module
 
+Data table structure:
+    * id (string): Unique and random generated identifier
+        at least 2 special characters (except: ';'), 2 number, 2 lower and 2 upper case letters)
+    * month (number): Month of the transaction
+    * day (number): Day of the transaction
+    * year (number): Year of the transaction
+    * type (string): in = income, out = outflow
+    * amount (int): amount of transaction in USD
+"""
 def run():
     """
     Starts this module and displays its menu.
@@ -12,8 +22,10 @@ def run():
     Returns:
         None
     """
-
-    options = ["Add record",
+    table = accounting.get_accounting_table_from_file()
+    title_list = ["ID", "Month", "Day", "Year", "Type","Amount"]
+    options = ["View records",
+               "Add record",
                "Remove record",
                "Update record",
                "Which year has the highest profit?",
@@ -23,16 +35,25 @@ def run():
     while choice != "0":
         choice = terminal_view.get_choice_inner_menu(options, "Accounting manager")
         if choice == "1":
-            store_controller.run()
+            terminal_view.print_table(table, title_list)
         elif choice == "2":
-            hr_controller.run()
+            record = terminal_view.get_inputs(title_list[1::],"Please provide new item data")
+            table = accounting.add(table, record)
         elif choice == "3":
-            inventory_controller.run()
+            id_to_delete_table = terminal_view.get_inputs(["ID"],"Item to delete")
+            id_to_delete = id_to_delete_table[0]
+            table = accounting.remove(table, id_to_delete)
         elif choice == "4":
-            accounting_controller.run()
+            records = terminal_view.get_inputs(title_list,"Edit item")
+            record_id = records[0]
+            table = accounting.update(table, record_id, records)
         elif choice == "5":
-            sales_controller.run()
+            max_year = accounting.which_year_max(table)
+            terminal_view.print_result(max_year, "Year of the highest profit")
         elif choice == "6":
-            crm_controller.run()
+            year_input = terminal_view.get_inputs(["Year"],"Average (per item) profit in a given year")
+            year = year_input[0]
+            avg_amount = accounting.avg_amount(table, year)
+            terminal_view.print_result(avg_amount,"Avarage profit in {}".format(year))
         elif choice != "0":
             terminal_view.print_error_message("There is no such choice.")
