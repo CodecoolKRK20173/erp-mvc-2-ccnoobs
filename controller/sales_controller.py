4c8c97ba9d1e65c4a8f4917f3e10c9b2b9462e97
Kamil Kiklica <kamil.kiklica@gmail.com>
2019-03-14 13:04:28 +0100

    Adedd foolproof for controller/sales_controller.py

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -54,13 +54,72 @@ def run():
             lowest_price = sales.get_lowest_price_item_id(table)
             terminal_view.print_result(lowest_price, "ID of item that was sold for the lowest price")
         elif choice == "6":
-            year_input = terminal_view.get_inputs(["Day from","Month from","Year from","Day to","Month to","Year to"],"Put dates between which you want to search")
-            month_from = year_input[1]
-            day_from = year_input[0]
-            year_from = year_input[2]
-            month_to = year_input[4]
-            day_to = year_input[3]
-            year_to = year_input[5]
+            day_from_input = terminal_view.get_inputs(["Day from"],"")
+            while not day_from_input[0].isdigit():
+                day_from_input = terminal_view.get_inputs(["Your input is not digit. Choose correct day from range 1-31"],"")
+            while day_from_input[0].isdigit():
+                while int(day_from_input[0]) > 31 or int(day_from_input[0]) < 1:
+                    day_from_input = terminal_view.get_inputs(["Choose correct day from range 1-31"],"")
+                    while not day_from_input[0].isdigit():
+                        day_from_input = terminal_view.get_inputs(["Your input is not digit. Choose correct day from range 1-31"],"")
+                break
+            day_from = day_from_input[0]
+            
+            month_from_input = terminal_view.get_inputs(["Month from"],"")
+            while not month_from_input[0].isdigit():
+                month_from_input = terminal_view.get_inputs(["Your input is not digit. Choose correct month from range 1-12"],"")
+            while month_from_input[0].isdigit():            
+                while not int(month_from_input[0]) < 13 or not int(month_from_input[0]) > 0:
+                    month_from_input = terminal_view.get_inputs(["Choose correct month from range 1-12"],"")   
+                    while not month_from_input[0].isdigit():
+                        month_from_input = terminal_view.get_inputs(["Your input is not digit. Choose correct month from range 1-12"],"")
+                break 
+            month_from = month_from_input[0]
+            
+            year_from_input = terminal_view.get_inputs(["Year from"],"")
+            while not year_from_input[0].isdigit():
+                year_from_input = terminal_view.get_inputs(["Your input is not digit. Choose correct year"],"")
+            while year_from_input[0].isdigit():
+                while not int(year_from_input[0]) > 0:
+                    year_from_input = terminal_view.get_inputs(["Choose correct year"],"")
+                    while not year_from_input[0].isdigit():
+                        year_from_input = terminal_view.get_inputs(["Your input is not digit. Choose correct year"],"")
+                break
+            year_from = year_from_input[0]
+            
+            day_to_input = terminal_view.get_inputs(["Day to"],"")
+            while not day_to_input[0].isdigit():
+                day_to_input = terminal_view.get_inputs(["Your input is not digit. Choose correct day from range 1-31"],"")
+            while day_to_input[0].isdigit():
+                while not int(day_to_input[0]) < 32 or not int(day_to_input[0]) > 0:
+                    day_to_input = terminal_view.get_inputs(["Choose correct day from range 1-31"],"")
+                    while not day_to_input[0].isdigit():
+                        day_to_input = terminal_view.get_inputs(["Your input is not digit. Choose correct day from range 1-31"],"")
+                break
+            day_to = day_to_input[0]
+            
+            month_to_input = terminal_view.get_inputs(["Month to"],"")
+            while not month_to_input[0].isdigit():
+                month_to_input = terminal_view.get_inputs(["Your input is not digit. Choose correct month from range 1-12"],"") 
+            while month_to_input[0].isdigit():
+                while not int(month_to_input[0]) < 13 or not int(month_to_input[0]) > 0:
+                    month_to_input = terminal_view.get_inputs(["Choose correct month from range 1-12"],"")
+                    while not month_to_input[0].isdigit():
+                        month_to_input = terminal_view.get_inputs(["Your input is not digit. Choose correct month from range 1-12"],"") 
+                break
+            month_to = month_to_input[0]
+            
+            year_to_input = terminal_view.get_inputs(["Year to"],"")
+            while not year_to_input[0].isdigit():
+                year_to_input = terminal_view.get_inputs(["Your input is not digit. Choose correct year"],"")
+            while year_to_input[0].isdigit():
+                while not int(year_to_input[0]) > 0 or not int(year_to_input[0]) > int(year_from):
+                    year_to_input = terminal_view.get_inputs(["You can't choose year before 'Year from' value. Put correct year"],"")
+                    while not year_to_input[0].isdigit():
+                        year_to_input = terminal_view.get_inputs(["Your input is not digit. Choose correct year"],"")
+                break
+            year_to = year_to_input[0]
+
             items_sold_by_date = sales.get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to)
             terminal_view.print_table(items_sold_by_date,title_list)
         elif choice != "0":
